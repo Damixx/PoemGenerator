4b0eb8fcd4106b1988b59dad2895c7832fe8b03d
Damixx <damix.k@yandex.ru>
2016-07-22 21:23:45 +0500

    Init

---
+++ app/src/main/java/com/vocvark/poemgenerator/Poem.java
@@ -0,0 +1,23 @@
+package com.vocvark.poemgenerator;
+
+public class Poem {
+    private int lineCount;
+    private int wordsInLine;
+    private String meter;
+    private String[] words;
+
+    private String poem;
+
+    public Poem(int lineCount, int wordsInLine, String meter, String[] words) {
+        this.lineCount = lineCount;
+        this.wordsInLine = wordsInLine;
+        this.meter = meter;
+        this.words = words;
+    }
+
+    public void GeneratePoem() {
+
+    }
+
+    public String Poem() { return poem; }
+}
